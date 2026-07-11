80bf12ff014e8fa174ad673a4aa4dfe766a8b5f7
rayhancoy64 <rayhancoy64@gmail.com>
2026-07-11 14:05:22 +0700

    merombak kerangka awal cuma ada berada sekarang ada 4 halaman berfunsi dan semua fitur berjalan dengan baik

---
+++ data.js
@@ -0,0 +1,81 @@
+// file: data.js
+
+const databasegame = [
+    {
+        id: 1,
+        judul: "Elden ring1",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/sekoden"
+    },
+    {
+        id: 2,
+        judul: "Elden ring2",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/sekoden"
+    },
+    {
+        id: 3,
+        judul: "Elden ring3",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/sekoden"
+    },
+    {
+        id: 4,
+        judul: "Elden ring4",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/topdown"
+    },
+    {
+        id: 5,
+        judul: "Elden ring5",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/topdown"
+    },
+    {
+        id: 6,
+        judul: "Elden ring6",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/topdown .d."
+    },
+    {
+        id: 7,
+        judul: "evil",
+        kategori: "horror",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/topdown"
+    },
+    {
+        id: 8,
+        judul: "Elden ring8",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/topdown"
+    },
+    {
+        id: 9,
+        judul: "Elden ring9",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/topdown"
+    },
+    {
+        id: 10,
+        judul: "Elden ring10",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/topdown"
+    },
+    {
+        id: 11,
+        judul: "Elden ring11",
+        kategori: "rpg",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/topdown"
+    }
+];
