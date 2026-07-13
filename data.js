0aaaa849623a196dacd0dd2301106881022d0b7c
rayhancoy64 <rayhancoy64@gmail.com>
2026-07-13 19:12:17 +0700

    mengatur atau merombak css agar tampilannya bagus

--- data.js
+++ data.js
@@ -3,79 +3,130 @@
 const databasegame = [
     {
         id: 1,
-        judul: "Elden ring1",
+        judul: "Game RPG",
         kategori: "rpg",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/sekoden"
+        file: "game-main/sekoden",
+        status: "rilis"
     },
     {
         id: 2,
-        judul: "Elden ring2",
-        kategori: "rpg",
+        judul: "Game Aksi",
+        kategori: "aksi",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/sekoden"
+        file: "game-main/aksi-file",
+        status: "belum_rilis"
     },
     {
         id: 3,
-        judul: "Elden ring3",
-        kategori: "rpg",
+        judul: "Game Horor",
+        kategori: "horor",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/sekoden"
+        file: "game-main/horor-file",
+        status: "belum_rilis"
     },
     {
         id: 4,
-        judul: "Elden ring4",
-        kategori: "rpg",
+        judul: "Game Balapan",
+        kategori: "balapan",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/topdown"
+        file: "game-main/balapan-file",
+        status: "belum_rilis"
     },
     {
         id: 5,
-        judul: "Elden ring5",
-        kategori: "rpg",
+        judul: "Game Petualangan",
+        kategori: "petualangan",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/topdown"
+        file: "game-main/petualangan-file",
+        status: "belum_rilis"
     },
     {
         id: 6,
-        judul: "Elden ring6",
-        kategori: "rpg",
+        judul: "Game Bertahan Hidup",
+        kategori: "bertahan hidup",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/topdown .d."
+        file: "game-main/survival-file",
+        status: "belum_rilis"
     },
     {
         id: 7,
-        judul: "evil",
-        kategori: "horror",
+        judul: "Game Strategi",
+        kategori: "strategi",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/topdown"
+        file: "game-main/strategi-file",
+        status: "belum_rilis"
     },
     {
         id: 8,
-        judul: "Elden ring8",
-        kategori: "rpg",
+        judul: "Game Teka-teki",
+        kategori: "teka-teki",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/topdown"
+        file: "game-main/teka-teki-file",
+        status: "belum_rilis"
     },
     {
         id: 9,
-        judul: "Elden ring9",
-        kategori: "rpg",
+        judul: "Game Simulasi",
+        kategori: "simulasi",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/topdown"
+        file: "game-main/simulasi-file",
+        status: "belum_rilis"
     },
     {
         id: 10,
-        judul: "Elden ring10",
-        kategori: "rpg",
+        judul: "Game Olahraga",
+        kategori: "olahraga",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/topdown"
+        file: "game-main/olahraga-file",
+        status: "belum_rilis"
     },
     {
         id: 11,
-        judul: "Elden ring11",
-        kategori: "rpg",
+        judul: "Game Kartu",
+        kategori: "kartu",
         gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/topdown"
+        file: "game-main/kartu-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 12,
+        judul: "Game Papan",
+        kategori: "papan",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/papan-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 13,
+        judul: "Game Kasual",
+        kategori: "kasual",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/kasual-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 14,
+        judul: "Game Edukasi",
+        kategori: "edukasi",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/edukasi-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 15,
+        judul: "Game Tembak-tembakan",
+        kategori: "tembak-tembakan",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/tembak-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 16,
+        judul: "Game Arkade",
+        kategori: "arkade",
+        gambar: "gambar game/elden-ring.jpg",
+        file: "game-main/arkade-file",
+        status: "belum_rilis"
     }
 ];
