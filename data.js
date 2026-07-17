f63a4bd64793b6297893e3ee7eae9d55307db2e1
rayhancoy64 <rayhancoy64@gmail.com>
2026-07-17 20:32:15 +0700

    menambahkan game dan gambar game sementara

--- data.js
+++ data.js
@@ -3,130 +3,185 @@
 const databasegame = [
     {
         id: 1,
-        judul: "Game RPG",
-        kategori: "rpg",
-        gambar: "gambar game/elden-ring.jpg",
+        judul: "Flappy Bird",
+        kategori: "kasual",
+        gambar: "gambar game/flappy_bird_1.png",
+        promo_gmbr: "promo_gambar/flappy_bird.jpg",
         file: "game-main/flapy_bird",
         status: "rilis"
     },
     {
         id: 2,
-        judul: "Game Aksi",
+        judul: "Hollow Knight Silksong",
         kategori: "aksi",
-        gambar: "gambar game/elden-ring.jpg",
+        gambar: "gambar game/hollow_knight_silksong.jpg",
+        promo_gmbr: "promo_gambar/hollow_knight_silksong.jpg",
         file: "game-main/aksi-file",
         status: "belum_rilis"
     },
     {
         id: 3,
-        judul: "Game Horor",
+        judul: "Resident Evil 2",
         kategori: "horor",
-        gambar: "gambar game/elden-ring.jpg",
+        gambar: "gambar game/resident_evil_2_p.jpg",
+        promo_gmbr: "promo_gambar/resident_evil.jpg",
         file: "game-main/horor-file",
         status: "belum_rilis"
     },
     {
         id: 4,
-        judul: "Game Balapan",
-        kategori: "balapan",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/balapan-file",
-        status: "belum_rilis"
+        judul: "Astro Dodge",
+        kategori: "kasual",
+        gambar: "gambar game/astro_dodge.png",
+        promo_gmbr: "promo_gambar/astro_dodge.png",
+        file: "game-main/astrododge",
+        status: "rilis"
     },
     {
         id: 5,
-        judul: "Game Petualangan",
+        judul: "Peak",
         kategori: "petualangan",
-        gambar: "gambar game/elden-ring.jpg",
+        gambar: "gambar game/peak.jpg",
+        promo_gmbr: "promo_gambar/peak.jpg",
         file: "game-main/petualangan-file",
         status: "belum_rilis"
     },
     {
         id: 6,
-        judul: "Game Bertahan Hidup",
+        judul: "Resident Evil 4",
         kategori: "bertahan hidup",
-        gambar: "gambar game/elden-ring.jpg",
+        gambar: "gambar game/resident_evil_4.jpg",
+        promo_gmbr: "promo_gambar/resident_evil_4.jpg",
         file: "game-main/survival-file",
         status: "belum_rilis"
     },
     {
         id: 7,
-        judul: "Game Strategi",
-        kategori: "strategi",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/strategi-file",
-        status: "belum_rilis"
-    },
-    {
-        id: 8,
-        judul: "Game Teka-teki",
-        kategori: "teka-teki",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/teka-teki-file",
-        status: "belum_rilis"
-    },
-    {
-        id: 9,
-        judul: "Game Simulasi",
+        judul: "Stray",
         kategori: "simulasi",
-        gambar: "gambar game/elden-ring.jpg",
+        gambar: "gambar game/stray.jpg",
+        promo_gmbr: "promo_gambar/stray.jpg",
         file: "game-main/simulasi-file",
         status: "belum_rilis"
     },
     {
+        id: 8,
+        judul: "Dark Souls",
+        kategori: "rpg",
+        gambar: "gambar game/dark_souls.jpg",
+        file: "game-main/rpg-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 9,
+        judul: "Celeste",
+        kategori: "arkade",
+        gambar: "gambar game/celeste.jpg",
+        file: "game-main/arkade-file",
+        status: "belum_rilis"
+    },
+    {
         id: 10,
-        judul: "Game Olahraga",
-        kategori: "olahraga",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/olahraga-file",
+        judul: "Elden Ring",
+        kategori: "rpg",
+        gambar: "gambar game/elden_ring.jpg",
+        file: "game-main/rpg-file",
         status: "belum_rilis"
     },
     {
         id: 11,
-        judul: "Game Kartu",
-        kategori: "kartu",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/kartu-file",
+        judul: "Hogwarts Legacy",
+        kategori: "rpg",
+        gambar: "gambar game/hogwarts_legacy.jpg",
+        file: "game-main/rpg-file",
         status: "belum_rilis"
     },
     {
         id: 12,
-        judul: "Game Papan",
-        kategori: "papan",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/papan-file",
-        status: "belum_rilis"
-    },
-    {
-        id: 13,
-        judul: "Game Kasual",
-        kategori: "kasual",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/kasual-file",
-        status: "belum_rilis"
-    },
-    {
-        id: 14,
-        judul: "Game Edukasi",
+        judul: "Minecraft",
         kategori: "edukasi",
-        gambar: "gambar game/elden-ring.jpg",
+        gambar: "gambar game/minecraft.jpg",
         file: "game-main/edukasi-file",
         status: "belum_rilis"
     },
     {
+        id: 13,
+        judul: "The Legend Of Zelda BOTW",
+        kategori: "petualangan",
+        gambar: "gambar game/the_legend_of_zelda_breath_of_the_wild.jpg",
+        file: "game-main/rpg-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 14,
+        judul: "The Last Of Us",
+        kategori: "bertahan hidup",
+        gambar: "gambar game/the_last_of_us.jpg",
+        file: "game-main/survival-file",
+        status: "belum_rilis"
+    },
+    {
         id: 15,
-        judul: "Game Tembak-tembakan",
-        kategori: "tembak-tembakan",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/tembak-file",
+        judul: "Red Dead Redemption",
+        kategori: "petualangan",
+        gambar: "gambar game/red_dead_redemption.jpg",
+        file: "game-main/petualangan-file",
         status: "belum_rilis"
     },
     {
         id: 16,
-        judul: "Game Arkade",
-        kategori: "arkade",
-        gambar: "gambar game/elden-ring.jpg",
-        file: "game-main/arkade-file",
+        judul: "Forza Horizon 5",
+        kategori: "balapan",
+        gambar: "gambar game/forza_horizon_5.jpg",
+        file: "game-main/balapan-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 17,
+        judul: "Battlefield 1",
+        kategori: "tembak-tembakan",
+        gambar: "gambar game/battlefield_1.jpg",
+        file: "game-main/tembak-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 18,
+        judul: "Little_Nightmares",
+        kategori: "teka-teki",
+        gambar: "gambar game/little_nightmares.jpg",
+        file: "game-main/teka-teki-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 19,
+        judul: "Age Empires 2",
+        kategori: "strategi",
+        gambar: "gambar game/age_empires_2.jpg",
+        file: "game-main/strategi-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 20,
+        judul: "efootball",
+        kategori: "olahraga",
+        gambar: "gambar game/efootball.jpg",
+        file: "game-main/olahraga-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 21,
+        judul: "Balatro",
+        kategori: "kartu",
+        gambar: "gambar game/balatro.jpg",
+        file: "game-main/kartu-file",
+        status: "belum_rilis"
+    },
+    {
+        id: 22,
+        judul: "Everdell",
+        kategori: "papan",
+        gambar: "gambar game/everdell.jpg",
+        file: "game-main/papan-file",
         status: "belum_rilis"
     }
 ];
